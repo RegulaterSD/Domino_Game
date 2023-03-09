bf5c18407ba161d5da22a2600591de3b7b2d3a78
Sean Davies <seandavies@unm.edu>
2023-03-09 11:09:33 -0700

    Added Basis of GUI

--- src/Project3b_seandavies.java
+++ src/Project3b_seandavies.java
@@ -1,2 +1,50 @@
-public class Project3b_seandavies {
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.time.Duration;
+import java.util.LinkedList;
+
+public class Project3b_seandavies extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Dominoes");
+        Canvas canvas = new Canvas(500, 250);
+        StackPane stackPane = new StackPane();
+        VBox root = new VBox();
+
+        AnimationTimer animationTimer = new AnimationTimer() {
+            private long startTime = -1;
+            @Override
+            public void handle(long now) {
+                if (startTime < 0){
+                    startTime = now;
+                }
+                Duration elapsed = Duration.ofNanos(now - startTime);
+                long milliseconds = elapsed.toMillis();
+
+            }
+        };
+
+
+
+        stage.setScene(new Scene(root,500,500));
+        stackPane.getChildren().add(canvas);
+        root.getChildren().add(stackPane);
+        stage.show();
+
+    }
 }
