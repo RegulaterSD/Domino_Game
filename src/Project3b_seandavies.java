4c1f9175c447d4f50a0950dc775bfc0829993e62
Sean Davies <seandavies@unm.edu>
2023-03-09 11:52:50 -0700

    Added variables to help draw dominoes

--- src/Project3b_seandavies.java
+++ src/Project3b_seandavies.java
@@ -30,13 +30,13 @@ public class Project3b_seandavies extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Dominoes");
-        Canvas canvas = new Canvas(900, 250);
+        Canvas canvas = new Canvas(1500, 250);
         StackPane stackPane = new StackPane();
         VBox root = new VBox();
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.rgb(255,255,255));
-        gc.fillRect(0,0,900,250);
+        gc.fillRect(0,0,1500,250);
 
         Label boneyardLabel = new Label("Boneyard contains " + " dominoes");
         boneyardLabel.setStyle("-fx-font: 24 arial;");
@@ -72,7 +72,7 @@ public class Project3b_seandavies extends Application {
 
 
 
-        stage.setScene(new Scene(root,900,350));
+        stage.setScene(new Scene(root,1500,350));
         root.getChildren().add(boneyardLabel);
         root.getChildren().add(computerLabel);
         stackPane.getChildren().add(canvas);
@@ -84,16 +84,27 @@ public class Project3b_seandavies extends Application {
 
     //LinkedList board, LinkedList playerDominoes
     static void drawDominoes(Canvas canvas){
+        int dominoWidth = 100;
+        int dominoHeight = 50;
+        int dotRadius = 10;
+        int lineWidth = 4;
+        int lineStart = 48;
+        int boardOne = 50;
+        int boardTwo = 100;
+        int boardStart = 50;
+        int userDepth = 200;
+        int userStart = 50;
+
         GraphicsContext gc = canvas.getGraphicsContext2D();
         Color dominoColor = Color.rgb(222,185,101);
         Color dotColor = Color.rgb(0,0,255);
         Color lineColor = Color.rgb(0,0,0);
         gc.setFill(dominoColor);
-        gc.fillRoundRect(450,100,100,50,10,10);
+        gc.fillRoundRect(boardStart,boardOne,dominoWidth,dominoHeight,dotRadius,dotRadius);
         gc.setFill(dotColor);
-        gc.fillOval(470,120,10,10);
+        gc.fillOval(boardStart + 20,boardOne + 20,dotRadius,dotRadius);
         gc.setFill(lineColor);
-        gc.fillRect(498,100,4,50);
+        gc.fillRect(boardStart + lineStart,boardOne,lineWidth,dominoHeight);
 
 
     }
